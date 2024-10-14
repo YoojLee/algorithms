b503e99ca90cae066bb8e01b651e3e5c52cb0703
YoojLee <daebaq27@gmail.com>
2024-10-14 16:19:14 +0900

    day 14

---
+++ guhyeon/string_rearrange.py
@@ -0,0 +1,17 @@
+def solution(N):
+    N = sorted(N)
+    alpha = ""
+    numeric = 0
+    for c in N:
+        if c.isalpha():
+            alpha += c
+        else: # number
+            numeric += int(c)
+    
+    return alpha+str(numeric)
+
+
+
+if __name__ == "__main__":
+    N = input()
+    print(solution(N))
