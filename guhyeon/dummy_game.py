cb252a91d60dd4c43fbd7e5a291f66cce0e4777a
YoojLee <daebaq27@gmail.com>
2024-10-19 01:25:31 +0900

    day 18

--- guhyeon/dummy_game.py
+++ guhyeon/dummy_game.py
@@ -70,7 +70,7 @@
 n = int(input())
 k = int(input())
 data = [[0] * (n+1) for _ in range(n+1)] # 맵 정보
-info = []
+info = [] # 회전 정보
 
 for _ in range(k):
     a,b = map(int, input().split())
@@ -93,27 +93,27 @@ def turn(direction, C):
     return direction
 
 def simulate():
-    x, y = 1, 1
-    data[x][y] = 2
+    x, y = 1, 1 # 처음 위치
+    data[x][y] = 2 # 뱀이 있는 곳은 표시함
     direction = 0
     time = 0
     index = 0
-    q = [(x,y)]
+    q = [(x,y)] # 현재 위치 -> 위치를 queue로 가져가면 되는 거구나
 
     while True:
         nx = x + dx[direction]
         ny = y + dy[direction]
 
-        if 1 <= nx and nx <= n and 1 <= ny and ny <= n and data[nx][ny] != 2:
-            if data[nx][ny] == 0:
-                data[nx][ny] = 2
-                q.append((nx, ny))
-                px, py = q.pop(0)
-                data[px][py] = 0
+        if 1 <= nx and nx <= n and 1 <= ny and ny <= n and data[nx][ny] != 2: # 뱀이 있는 곳을 표시해야 자기 자신에 부딪힌 것을 알 수 있음
+            if data[nx][ny] == 0: # 사과가 없다면
+                data[nx][ny] = 2 # 일단 방문 처리
+                q.append((nx, ny)) # 방문한 곳 넣어주기 (이게 어차피 꺾이는 거니까 좌표를 다 넣어주는 게 유리한 것 같다) => 요걸 이렇게 관리하는 게 중요할 듯
+                px, py = q.pop(0) # 기존의 tail
+                data[px][py] = 0 # 한 칸 움직이기 -> tail이 길어지지 않고 이걸 그냥 움직이는 형태
 
-            if data[nx][ny] == 1:
-                data[nx][ny] = 2
-                q.append((nx, ny))
+            if data[nx][ny] == 1: # 사과가 있다면 -> 길이만 늘림
+                data[nx][ny] = 2 # 일단 방문 처리
+                q.append((nx, ny)) # 방문한 곳 넣어주기 -> 대신 tail을 움직이진 않음
         else:
             time += 1
             break
@@ -122,7 +122,7 @@ def simulate():
         time += 1
         if index <l and time == info[index][0]:
             direction = turn(direction, info[index][1])
-            index += 1
+            index += 1 # 이걸로 약간 queue 같은 역할을 했다고 봄 -> 얘도 queue로 구현할 수 있지 않을까 싶음
     return time
 
 print(simulate())
